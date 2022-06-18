ca887e814b8e48cd862d7217c60500855b49b238
mibrahim234 <mohamm1417@gmail.com>
2022-06-17 20:24:50 -0400

    setting up styles for header

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,1 +1,10 @@
 import { makeStyles } from '@material-ui/core/styles'
+// How classes work in MUI?
+// import it in styles.js file 
+
+// Export styles
+// function with callback function which returns an object
+// whatever styles you want to add you can add them the way you add css to a js file 
+export default makeStyles(() => ({
+    title: {}
+}))
