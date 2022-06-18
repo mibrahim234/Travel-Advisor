95c3724c48de6a6d5f83efd203f7592db6a8c724
mibrahim234 <mohamm1417@gmail.com>
2022-06-17 20:12:08 -0400

    import search icon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,6 +10,19 @@ const Header = () => {
         <Typography variant='h5' className={classes.title}>
         Travel Advisor 
         </Typography>
+        {/* In mui a box is like a div */}
+        <Box display="flex">
+            <Typography variant='h6' className={classes.title}>
+            Explore new places
+            </Typography>
+            <Autocomplete>
+               <div className={classes.search}>
+                <div className={classes.searchIcon}>
+                    <SearchIcon />
+                    </div>
+                </div> 
+            </Autocomplete>
+        </Box>
         </Toolbar> 
 
        </AppBar>
