4442e28033828fd4b35a2b3a4407d92f671a91ed
mibrahim234 <mohamm1417@gmail.com>
2022-06-20 15:21:42 -0400

    styles for header

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,10 +1,38 @@
-import { makeStyles } from '@material-ui/core/styles'
+import { alpha, makeStyles } from '@material-ui/core/styles'
 // How classes work in MUI?
 // import it in styles.js file 
 
 // Export styles
 // function with callback function which returns an object
 // whatever styles you want to add you can add them the way you add css to a js file 
-export default makeStyles(() => ({
-    title: {}
-}))
+export default makeStyles((theme) => ({
+    title: {
+      display: 'none',
+      [theme.breakpoints.up('sm')]: {
+        display: 'block',
+      },
+    },
+    search: {
+      position: 'relative',
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: alpha(theme.palette.common.white, 0.15),
+      '&:hover': { backgroundColor: alpha(theme.palette.common.white, 0.25) },
+      marginRight: theme.spacing(2),
+      marginLeft: 0,
+      width: '100%',
+      [theme.breakpoints.up('sm')]: { marginLeft: theme.spacing(3), width: 'auto' },
+    },
+    searchIcon: {
+      padding: theme.spacing(0, 2), height: '100%', position: 'absolute', pointerEvents: 'none', display: 'flex', alignItems: 'center', justifyContent: 'center',
+    },
+    inputRoot: {
+      color: 'inherit',
+    },
+    inputInput: {
+      padding: theme.spacing(1, 1, 1, 0), paddingLeft: `calc(1em + ${theme.spacing(4)}px)`, transition: theme.transitions.create('width'), width: '100%', [theme.breakpoints.up('md')]: { width: '20ch' },
+    },
+    toolbar: {
+      display: 'flex', justifyContent: 'space-between',
+    },
+  }));
+  
