0419dec9f8e0530bbf90650418cc0ec4c2642f3e
mibrahim234 <mohamm1417@gmail.com>
2022-06-21 18:16:50 -0400

    setup the list form add state

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -6,9 +6,18 @@ import useStyles from './styles';
 
 const List = () => {
     const classes = useStyles();
-    
     return (
-        <h1>List</h1>
+        <div className={classes.container}>
+            <Typography variant="h4">Restaurants, Hotels & Attractions around you</Typography>
+            <FormControl className={classes.formControl}>
+                <InputLabel>Type</InputLabel>
+                <Select value={} onChange={}>
+                    <MenuItem value="restaurants"></MenuItem>
+                    <MenuItem value="hotels"></MenuItem>
+                    <MenuItem value="attractions"></MenuItem>
+                </Select>
+            </FormControl>
+        </div>
     );
 }
 
