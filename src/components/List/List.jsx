54f46fc700002ffd8b6b9d1ee34eb8581e4aadc0
mibrahim234 <mohamm1417@gmail.com>
2022-06-21 18:26:05 -0400

    set up rating systme and state

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 // circularprogress is loading bar 
 import { CircularProgress, Grid, Typography, MenuItem, FormControl, Select } from '@material-ui/core';
 
@@ -6,15 +6,32 @@ import useStyles from './styles';
 
 const List = () => {
     const classes = useStyles();
+    // first thing is the state, second thing is a function that modifies the state, accepts one param
+    const [type, setType] = useState('restaurants');
+    const [rating, setRating] = useState('');
+
     return (
         <div className={classes.container}>
             <Typography variant="h4">Restaurants, Hotels & Attractions around you</Typography>
             <FormControl className={classes.formControl}>
                 <InputLabel>Type</InputLabel>
-                <Select value={} onChange={}>
-                    <MenuItem value="restaurants"></MenuItem>
-                    <MenuItem value="hotels"></MenuItem>
-                    <MenuItem value="attractions"></MenuItem>
+          {/* Select data has to be modified using state */}
+          {/* onChange: gives us a callback function that has the event as the parameter */}
+          {/* e.target.value will be populated with the data the user searches for */}
+     <Select value={type} onChange={(e) => setType(e.target.value)}>
+                    <MenuItem value="restaurants">Restaurants</MenuItem>
+                    <MenuItem value="hotels">Hotels</MenuItem>
+                    <MenuItem value="attractions">Attractions</MenuItem>
+                </Select>
+            </FormControl>
+
+                    {/* Rating formcontrol */}
+            <FormControl className={classes.formControl}>
+                <InputLabel>Type</InputLabel>
+     <Select value={type} onChange={(e) => setType(e.target.value)}>
+                    <MenuItem value="restaurants">Restaurants</MenuItem>
+                    <MenuItem value="hotels">Hotels</MenuItem>
+                    <MenuItem value="attractions">Attractions</MenuItem>
                 </Select>
             </FormControl>
         </div>
