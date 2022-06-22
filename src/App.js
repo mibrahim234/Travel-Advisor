db73038a413ccdd369249011bfb4f6ad39cc10bd
mibrahim234 <mohamm1417@gmail.com>
2022-06-22 11:42:28 -0400

    added axios call to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 // CssBaseline: component from mui that fixes padding bg colors for us 
 import { CssBaseline, Grid } from '@material-ui/core';
+
+import { getPlacesData } from './api';
 import Header from './components/Header/Header';
 import List from './components/List/List';
 import Map from './components/Map/Map';
@@ -8,6 +10,20 @@ import Map from './components/Map/Map';
 
 // functional app component 
 const App= () => {
+    // state for restuarant data 
+    const [places, setPlaces] = useState([]);
+
+    // useEffect function that accepts another callback function
+    // if dependency array is empty code will only run at the start 
+    // getplacesdata is a async function so we have to call .then 
+    useEffect(() => {
+        getPlacesData()
+        // we grab the data with .then and sent them to the state 
+            .then((data) => {
+                console.log(data);
+                setPlaces(data);
+            })
+    }, []);
     return (
         // Call the components 
         <>
