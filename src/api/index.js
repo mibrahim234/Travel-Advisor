ebcd3923193ff4eb4848617470105d1d25e0efd7
mibrahim234 <mohamm1417@gmail.com>
2022-06-22 11:33:28 -0400

    axios function ready

--- src/api/index.js
+++ src/api/index.js
@@ -1,21 +1,14 @@
 // helps us make api calls
 import axios from 'axios'; 
 
+const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
+
 const options = {
-    method: 'GET',
-    url: 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary',
     params: {
       bl_latitude: '11.847676',
       tr_latitude: '12.838442',
       bl_longitude: '109.095887',
       tr_longitude: '109.149359',
-      restaurant_tagcategory_standalone: '10591',
-      restaurant_tagcategory: '10591',
-      limit: '30',
-      currency: 'USD',
-      open_now: 'false',
-      lunit: 'km',
-      lang: 'en_US'
     },
     headers: {
       'X-RapidAPI-Key': 'c3d4e748ddmsh0a4b9dabe4343cap1a7829jsn7145037507c8',
@@ -29,11 +22,14 @@ const options = {
       console.error(error);
   });
 
-const getPlacesData = async () => {
+export const getPlacesData = async () => {
     try {
         // request
-        const response = await axios.get();
-    } catch (errpr) {
-
+        // more interested in data from response so we destructure 
+        // url and options is parameters
+        const { data: { data }} = await axios.get(URL, options);
+        return data; 
+    } catch (error) {
+        console.log(error)
     }
 }
