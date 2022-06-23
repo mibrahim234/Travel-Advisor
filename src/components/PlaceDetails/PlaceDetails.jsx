4c0c1515696eb6d2c31e879ddbb421c2b5a6089f
mibrahim234 <mohamm1417@gmail.com>
2022-06-23 08:56:54 -0400

    fixing up card component

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -10,7 +10,19 @@ import useStyles from './styles';
 const PlaceDetails = ({ place }) => {
     const classes = useStyles(); 
     return (
-    <Card></Card>
+        // elevation will give it a nice shadow effect 
+    <Card elevation={6}>
+        <CardMedia 
+            style={{ height: 350 }}
+            image={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'} 
+            title={place.name }
+        />
+    <CardContent>
+        {/* Gutterbtm means margin bottom */}
+        <Typography gutterBottom variant="h5">{place.name}</Typography>
+
+    </CardContent>
+    </Card>
         );
 }
 
